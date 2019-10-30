1e9cdc81dc3abacef29d87d7b59e0dff626f4ba3
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2019-10-29 22:58:00 -0400

    styling

--- src/MemberDetails.tsx
+++ src/MemberDetails.tsx
@@ -7,13 +7,24 @@ interface MemberDetailsProps {
   name: string,
 }
 
-const StyledMemberDetails = styled.div<{brushColor: string}>`
-  background-color: black;
+const StyledMemberDetails = styled.div<{ brushColor: string }>`
+  background-color: grey;
+  border-radius: 20px;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  flex-direction: column;
   .color-circle{
     background-color: ${props => props.brushColor};
     height: 10px;
     width: 10px;
     border-radius: 50%;
+    display: inline-block;
+    align-self: center;
+  }
+  .color-container{
+    display: flex;
+    width: 75%;
   }
 `
 
@@ -21,12 +32,12 @@ const MemberDetails: React.FC<MemberDetailsProps> = (props) => {
   const { color, primary, name } = props;
   return (
     <StyledMemberDetails brushColor={"chartreuse"}>
-      <p>{name}</p>
-      <p>{primary ? "Primary" : "Dependent"}</p>
-      <div>
+      <div>{name}</div>
+      <div>{primary ? "Primary" : "Dependent"}</div>
+      <div className="color-container">
         <div className="color-circle"></div>
+        <div>{color}</div>
       </div>
-      <p>{color}</p>
     </StyledMemberDetails>
   )
 
