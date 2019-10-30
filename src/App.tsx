e7f6d5436301aace55ad2647239f26e1309ba2fc
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2019-10-30 17:40:02 -0400

    clean up App

--- src/App.tsx
+++ src/App.tsx
@@ -13,31 +13,8 @@ const App: React.FC = () => {
   console.log(response, loading, error)
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       {error && <div id="box"></div>}
       {!loading && <MemberDetails color="Blue" name="tim" primary={true}/>}
-      <div>
-        <button
-          onClick={() => {
-            setstate(state + 1);
-          }}
-        >
-          {state}
-        </button>
-      </div>
     </div>
   );
 };
