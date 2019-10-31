fc2113c53055cd5f0d8f236f022fca677ef48d92
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2019-10-31 19:02:05 -0400

    tab styling WIP

--- src/components/shipping/Shipping.tsx
+++ src/components/shipping/Shipping.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import TabHeader from "./TabHeader";
+
+const STARTER_BOXES = "Starter Boxes";
+const REFIL_BOXES = "Refill Boxes";
 
 interface ShippingProps {
   setOnShippingPage: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,6 +12,9 @@ const StyledShipping = styled.div<{}>`
   .link-text {
     color: #01c9f0;
     cursor: pointer;
+  }
+  .tab-container{
+    display: flex;
   }
 `;
 const Shipping: React.FC<ShippingProps> = props => {
@@ -19,8 +26,17 @@ const Shipping: React.FC<ShippingProps> = props => {
       <a className="link-text" onClick={e => setOnShippingPage(false)}>
         Back to Faimly Brush Preferences
       </a>
-      <div>shipping container!</div>
-      <div>
+      <div className="tab-container">
+        <TabHeader
+          headerText={STARTER_BOXES}
+          handleClick={setTabSelection}
+          selected={STARTER_BOXES === tabSelection}
+        />
+        <TabHeader
+          headerText={REFIL_BOXES}
+          handleClick={setTabSelection}
+          selected={REFIL_BOXES === tabSelection}
+        />
       </div>
     </StyledShipping>
   );
--- src/components/shipping/TabHeader.tsx
+++ src/components/shipping/TabHeader.tsx
@@ -3,15 +3,26 @@ import styled from "styled-components";
 
 interface TabHeaderProps {
   headerText: string;
-  handleClick: () => void;
+  handleClick: React.Dispatch<React.SetStateAction<string>>;
+  selected: boolean;
 }
-const StyledTabHeader = styled.div<{}>`
+const StyledTabHeader = styled.div<{ selected: boolean }>`
+  background-color: ${props => (props.selected ? "white" : "#f7f7f7")};
+  cursor: pointer;
+  width: 300px;
+  height: 100px;
+  border: 1px solid grey;
+  font-size: 1.7em;
   color: #01c9f0;
+  .button-text {
+    color: #01c9f0;
+  }
 `;
 const TabHeader: React.FC<TabHeaderProps> = props => {
+  const { headerText, selected, handleClick } = props;
   return (
-    <StyledTabHeader>
-      <div>aasdf</div>
+    <StyledTabHeader selected={selected} onClick={e => handleClick(headerText)}>
+      {headerText}
     </StyledTabHeader>
   );
 };
